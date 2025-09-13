b093785ce5dcbb1bb1af94e599fa81e07f621c38
unknown <gateraprincemarvin@gmail.com>
2025-09-13 18:09:15 +0200

    square

---
+++ 8-square.js
@@ -0,0 +1,13 @@
+// 8-square.js
+
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+if (isNaN(size)) {
+  console.log("Missing size");
+} else if (size > 0) {
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
+}
+// If size is zero or negative, do nothing
